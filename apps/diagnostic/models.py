f3ce8236932dab81a4efdca73e9792fb1331a64c
FreddyxD5 <freddyxd5@gmail.com>
2023-03-02 13:10:38 -0500

    Added vet to Diagnostic model

--- apps/diagnostic/models.py
+++ apps/diagnostic/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from apps.base.models import BaseModel
 from apps.pet.models import Pet
+from apps.users.models import Usuario
 
 
 # Create your models here.
@@ -8,7 +10,7 @@ from apps.pet.models import Pet
 
 class Diagnostic(BaseModel):
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
-    # doctor = models.ForeignKey(Employee, on_delete=models.CASCADE)    
+    vet = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
     observations = models.TextField()
     medication = models.TextField()
     instruction = models.TextField()
@@ -17,8 +19,10 @@ class Diagnostic(BaseModel):
     def __str__(self):
         return f"Diagnostic{self.pet.name} - {self.pet.owner.nombres}"
     
+    def save(self,*args,**kwargs):
+        if self.vet.role != 'VET':
+            raise ValidationError(_('Debe asignar un veterinario'), code='role_error')
+        super(Diagnostic, self).save(*args, **kwargs)
 
-#class DiagnosticDetail(BaseModel):
-#    diagnostic = models.ForeignKey(Diagnostic, on_delete=models.CASCADE)
-    
+
 
