2d02444cb93386c630a0519dadd9f35086640360
FreddyxD5 <freddyxd5@gmail.com>
2023-03-20 17:38:45 -0500

    Added diagnostic view + simpleJWT

--- apps/diagnostic/models.py
+++ apps/diagnostic/models.py
@@ -6,23 +6,22 @@ from apps.users.models import Usuario
 
 
 # Create your models here.
-
+    
 
 class Diagnostic(BaseModel):
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
-    vet = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
+    vet = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, related_name="diagnostics")
     observations = models.TextField()
     medication = models.TextField()
     instruction = models.TextField()
-    active = models.TextField()
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"Diagnostic{self.pet.name} - {self.pet.owner.nombres}"
     
     def save(self,*args,**kwargs):
-        if self.vet.role != 'VET':
-            raise ValidationError(_('Debe asignar un veterinario'), code='role_error')
+        if self.vet.role != 'Veterinary':
+            raise ValidationError(('Debe asignar un veterinario'), code='role_error')
         super(Diagnostic, self).save(*args, **kwargs)
-
 
 
